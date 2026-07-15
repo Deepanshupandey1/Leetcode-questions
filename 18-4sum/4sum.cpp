d0bf2fa79b960eb4e8a087ff92fa4416fc1fb05a
Deepanshu pandey <deepanshu11pandey@gmail.com>
2026-07-15 18:43:44 +0530

    Time: 19 ms (80.96%) | Memory: 17.6 MB (23.59%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,81 @@
+class Solution
+{
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target)
+    {
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        int i = 0;
+
+        while(i < n - 3)
+        {
+            int j = i + 1;
+
+            while(j < n - 2)
+            {
+                int left = j + 1;
+                int right = n - 1;
+
+                while(left < right)
+                {
+                    long long sum = (long long)nums[i] +
+                                    nums[j] +
+                                    nums[left] +
+                                    nums[right];
+
+                    if(sum == target)
+                    {
+                        ans.push_back(
+                        {
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        });
+
+                        left++;
+                        right--;
+
+                        while(left < right && nums[left] == nums[left - 1])
+                        {
+                            left++;
+                        }
+
+                        while(left < right && nums[right] == nums[right + 1])
+                        {
+                            right--;
+                        }
+                    }
+                    else if(sum < target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+
+                j++;
+
+                while(j < n - 2 && nums[j] == nums[j - 1])
+                {
+                    j++;
+                }
+            }
+
+            i++;
+
+            while(i < n - 3 && nums[i] == nums[i - 1])
+            {
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
